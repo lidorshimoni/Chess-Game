964842f70cc958b26cf3074a603922eedb289fca
shilat <shilat_mm@walla.com>
2018-12-21 16:02:56 +0200

    somthing

--- Chess/Queen.cpp
+++ Chess/Queen.cpp
@@ -5,11 +5,11 @@ Queen::Queen(Position pos, char type, int color, Board *board) : Rook(pos, type,
 Queen::~Queen() {}
 
 vector<Position> Queen::getLeaglMoves() {
-	//vector<Position> v = this->Rook::getLeaglMoves();
-	//vector<Position> v2 = this->Bishop::getLeaglMoves();
-	//vector<Position> v3;
-	//v3.reserve(v.size() + v2.size());
-	//v3.insert(v3.end(), v.begin(), v.end());
-	//v3.insert(v3.end(), v2.begin(), v2.end());
-	//return v3;
+	vector<Position> v = this->Rook::getLeaglMoves();
+	vector<Position> v2 = this->Bishop::getLeaglMoves();
+	vector<Position> v3;
+	v3.reserve(v.size() + v2.size());
+	v3.insert(v3.end(), v.begin(), v.end());
+	v3.insert(v3.end(), v2.begin(), v2.end());
+	return v3;
 }
